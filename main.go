1cc897c7bcf76f93ac36607047219a40d5d74081
Zaki Manian <zakimanian@zakis-mbp.lan>
2018-10-10 00:25:41 -0700

    Removing main buildinstructions

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package go_to_rust_ristretto
 
-/*
-#cgo darwin LDFLAGS: -L./lib -lhello_ristretto
-#include "./lib/hello_ristretto.h"
-*/
-import "C"
-
 func main() {
 	GenerateRistrettoPoint()
 }
